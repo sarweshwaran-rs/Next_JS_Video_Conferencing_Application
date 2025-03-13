5745651bed2fc2de7fd2eb963e7c380e525ca552
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-03-13 20:19:01 +0530

    Corrected the problem

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from 'next/server';
 
 const protectedRoutes = createRouteMatcher([
     '/',
@@ -13,8 +14,9 @@ const protectedRoutes = createRouteMatcher([
 export default clerkMiddleware((auth, req)=>{
   
   if (protectedRoutes(req)) {
-    return auth.protect();
+    return auth.protect().then(() => NextResponse.next()).catch(() => NextResponse.redirect('/sign-in'));
   } 
+  return NextResponse.next();
 });
 
 export const config = {
