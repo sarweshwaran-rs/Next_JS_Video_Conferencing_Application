a0e92de75d2b56cf8580870726d6049072569cda
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-03-13 20:33:59 +0530

    Added Appearance

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,13 +34,6 @@ export default function RootLayout({
             layout: {
               logoImageUrl: '/icons/yoom-logo.svg',
               socialButtonsVariant: 'iconButton'
-            },
-            variables: {
-              colorText: '#fff',
-              colorPrimary:'#0E78F9',
-              colorBackground: '#1c1f2e',
-              colorInputBackground: "#232a41",
-              colorInputText: '#fff',
             }
           }}
       >
