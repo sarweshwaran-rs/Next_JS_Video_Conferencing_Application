675ef2ff16e4884c951ddc29e5d43642c3aa19be
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-03-14 13:08:42 +0530

    Completed the Card

--- components/ui/MeetingList.tsx
+++ components/ui/MeetingList.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
 
 const MeetingList = () => {
   return (
-    <div>MeetingList</div>
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+        <div className = 'bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer'
+            onClick={()=>{}}
+        >
+            <div className='flex-center glassmorphism size-12 rounded-[10px] items-center'>
+                <Image src = '/icons/add-meeting.svg' alt="Metting" width={27} height={27} />
+            </div>
+            <div className='flex flex-col gap-2'>
+                <h1  className='text-2xl font-bold'>New Meeting</h1>
+                <p className='text-lg font-normal'>Start an Instant Meeting</p>
+            </div>
+        </div>
+
+        <div className='bg-purple-1'>
+            Box 2
+        </div>
+
+        <div className='bg-yellow-1'>
+            Box 3
+        </div>
+
+        <div className='bg-sky-4'>
+            Box 4       
+        </div>
+
+    </section>
   )
 }
 
