7c45e399e6df3d46f128f740747f09ae80944a3a
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-03-14 13:11:03 +0530

    Created the Home Card Component

---
+++ components/ui/HomeCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+
+const HomeCard = () => {
+    return (
+        <div className='bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer'
+            onClick={() => { }}
+        >
+            <div className='flex-center glassmorphism size-12 rounded-[10px] items-center'>
+                <Image src='/icons/add-meeting.svg' alt="Metting" width={27} height={27} />
+            </div>
+            <div className='flex flex-col gap-2'>
+                <h1 className='text-2xl font-bold'>New Meeting</h1>
+                <p className='text-lg font-normal'>Start an Instant Meeting</p>
+            </div>
+        </div>
+    )
+}
+
+export default HomeCard
