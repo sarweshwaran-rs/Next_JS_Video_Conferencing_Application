73f6ac3c316d8823ceb866f637c6708671189487
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-03-14 11:41:12 +0530

    Resolved the error

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,27 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from 'next/server';
 
 const protectedRoutes = createRouteMatcher([
-    '/',
-    '/upcoming',
-    '/previous',
-    '/recordings',
-    '/personal-room',
-    '/meeting(.*)',
+  '/(.*)',
+  '/upcoming(.*)',
+  '/previous(.*)',
+  '/recordings(.*)',
+  '/personal-room(.*)',
+  '/meeting(.*)'
 ])
 
 
-export default clerkMiddleware((auth, req)=>{
-  
+export default clerkMiddleware((auth, req) => {
+
+  if (req.nextUrl.pathname.startsWith("/sign-in") || req.nextUrl.pathname.startsWith("/sign-up")) {
+    return NextResponse.next();
+  }
+
   if (protectedRoutes(req)) {
-    return auth.protect().then(() => NextResponse.next()).catch(() => NextResponse.rewrite(new URL('/sign-in', req.url)));
-  } 
+    return auth
+      .protect()
+      .then(() =>NextResponse.next())
+      .catch(() => NextResponse.rewrite(new URL('/sign-in', req.url)));
+  }
   return NextResponse.next();
 });
 
