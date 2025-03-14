3a2c4345f53538665f5b582df1467c2db534b9a0
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-03-14 21:13:16 +0530

    Formatted Document

--- components/ui/MeetingList.tsx
+++ components/ui/MeetingList.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation'
 const MeetingList = () => {
 
     const router = useRouter();
-    
+
     const [meetingState, setMeetingState] = useState<'isScheculeMeeting' | 'isJoinMeeting' | 'isInstantMeeting' | undefined>()
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
@@ -15,22 +15,22 @@ const MeetingList = () => {
                 description='Start an Instant Meeting'
                 handleClick={() => setMeetingState('isJoinMeeting')}
                 className='bg-orange-1'
-        />
+            />
             <HomeCard
                 img='/icons/schedule.svg'
                 title='Schecule Meeting'
                 description='Plan your Meeting'
-                handleClick={() => setMeetingState('isScheculeMeeting')} 
-                className = 'bg-blue-1'           
+                handleClick={() => setMeetingState('isScheculeMeeting')}
+                className='bg-blue-1'
             />
-            <HomeCard 
+            <HomeCard
                 img='/icons/recordings.svg'
                 title='View Recordings'
                 description='Check out your recorded Meeting'
                 handleClick={() => router.push('/recordings')}
                 className='bg-purple-1'
             />
-            <HomeCard 
+            <HomeCard
                 img='/icons/join-meeting.svg'
                 title='Join Meeting'
                 description='Join Via Meeting Invitation Link'
