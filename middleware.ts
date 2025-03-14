df7bbe7a4830a9d27f21a0785887e90bb0bc46ed
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-03-14 11:22:51 +0530

    Removed the unuses import of the NextRequest

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextRequest ,NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 const protectedRoutes = createRouteMatcher([
     '/',
