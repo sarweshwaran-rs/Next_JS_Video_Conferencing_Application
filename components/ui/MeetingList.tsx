22a1c9d3529a1061170f178a071bdc603a6bcd78
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-04-05 18:38:31 +0530

    Created the button for instant meeting

--- components/ui/MeetingList.tsx
+++ components/ui/MeetingList.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModal from '../MeetingModal';
 
 const MeetingList = () => {
 
     const router = useRouter();
 
     const [meetingState, setMeetingState] = useState<'isScheculeMeeting' | 'isJoinMeeting' | 'isInstantMeeting' | undefined>()
+
+    const createMeeting = () => {
+
+    }
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
             <HomeCard
                 img='/icons/add-meeting.svg'
                 title='New Meeting'
                 description='Start an Instant Meeting'
-                handleClick={() => setMeetingState('isJoinMeeting')}
+                handleClick={() => setMeetingState('isInstantMeeting')}
                 className='bg-orange-1'
             />
             <HomeCard
@@ -34,11 +39,20 @@ const MeetingList = () => {
                 img='/icons/join-meeting.svg'
                 title='Join Meeting'
                 description='Join Via Meeting Invitation Link'
-                handleClick={() => setMeetingState('isInstantMeeting')}
+                handleClick={() => setMeetingState('isJoinMeeting')}
                 className='bg-yellow-1'
+            />
+            
+            <MeetingModal 
+                isOpen={meetingState === 'isInstantMeeting'}
+                onClose = {() => setMeetingState(undefined)}
+                title = "Start an Instant Meeting"
+                className = "text-center"
+                buttonText = "Start Meeting"
+                handleClick = {createMeeting}
             />
         </section>
     )
 }
-
+    
 export default MeetingList
