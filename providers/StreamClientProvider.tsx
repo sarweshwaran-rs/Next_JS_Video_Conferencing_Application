2617b80104dbd5954801b75b40200f22c3583a32
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-04-05 21:56:18 +0530

    Added the use client directory

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,3 +1,4 @@
+"use client"
 // import { StreamCall, StreamVideo, StreamVideoClient, User,} from "@stream-io/video-react-sdk";
 import { tokenProvider } from "@/actions/stream.actions";
 import Loader from "@/components/Loader";
