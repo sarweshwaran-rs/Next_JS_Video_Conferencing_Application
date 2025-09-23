ea3862b5e828dea1ba1e1e07223f01771818094a
= <sarweshchandran@gmail.com>
2025-09-23 14:42:59 +0530

    Refactored with flex-wrap

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -56,8 +56,9 @@ const MeetingRoom = () => {
                 </div>
             </div>
 
-            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
                 <CallControls />
+                
                 <DropdownMenu>
                     <div className="flex items-center">
                         <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
