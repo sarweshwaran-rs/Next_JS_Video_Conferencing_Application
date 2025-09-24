09d3d4c2ce7ba469685ffd7a5538b73812a8c212
= <sarweshchandran@gmail.com>
2025-09-24 18:55:46 +0530

    Added the previous calls info

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import CallList from '@/components/ui/CallList'
 
 const Previous = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
     <h1 className="text-3xl font-bold">
-      Previous
+      <CallList type='ended' />
     </h1>
   </section>
   )
