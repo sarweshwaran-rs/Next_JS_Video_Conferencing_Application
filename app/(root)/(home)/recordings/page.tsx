f2a5c35e29c4af259a0c6f184cb502af33016f09
= <sarweshchandran@gmail.com>
2025-09-24 18:56:28 +0530

    Added the recordings info

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import CallList from '@/components/ui/CallList'
 
 const Recordings = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
     <h1 className="text-3xl font-bold">
-      Recordings
+      <CallList type='recordings' />
     </h1>
   </section>
   )
