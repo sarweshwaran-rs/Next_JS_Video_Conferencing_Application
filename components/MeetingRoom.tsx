883c2cbad4dd2f02ef9ddf74e2e30ebd1ac59ac1
= <sarweshchandran@gmail.com>
2025-09-25 07:17:46 +0530

    Refactored the Meeting Room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -11,7 +11,7 @@ import {
 } from "../components/ui/dropdown-menu";
 
 import { Users, LayoutList } from 'lucide-react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import PersonalRoom from '@/app/(root)/(home)/personal-room/page';
 import EndCallButton from './EndCallButton';
 import Loader from './Loader';
@@ -22,6 +22,7 @@ const MeetingRoom = () => {
 
     const searchParams = useSearchParams();
     const isPersonalRoom = !!searchParams.get('personal');
+    const router = useRouter();
 
     const { useCallCallingState } = useCallStateHooks();
     const callingState = useCallCallingState();
@@ -57,7 +58,7 @@ const MeetingRoom = () => {
             </div>
 
             <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-                <CallControls />
+                <CallControls onLeave={() => router.push('/')}/>
                 
                 <DropdownMenu>
                     <div className="flex items-center">
